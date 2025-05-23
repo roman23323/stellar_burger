ceb7a6c7f4f1cb58203be64b6aad53d1f87b5804
Roman <ignatenko_roman.s@mail.ru>
2025-05-23 11:00:16 +0300

    feat: implement registration logic

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,25 +1,45 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { RootState, useDispatch, useSelector } from '../../services/store';
+import {
+  register,
+  selectAuthError,
+  selectAuthLoading
+} from '../../services/auth/slice';
+import { TRegisterData } from '@api';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const error = useSelector(selectAuthError);
+  const isLoading = useSelector(selectAuthLoading);
+  const dispatch = useDispatch();
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    const regData: TRegisterData = {
+      email,
+      name: userName,
+      password
+    };
+    dispatch(register(regData));
   };
 
   return (
-    <RegisterUI
-      errorText=''
-      email={email}
-      userName={userName}
-      password={password}
-      setEmail={setEmail}
-      setPassword={setPassword}
-      setUserName={setUserName}
-      handleSubmit={handleSubmit}
-    />
+    <>
+      <RegisterUI
+        errorText={error || ''}
+        email={email}
+        userName={userName}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        setUserName={setUserName}
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+      />
+    </>
   );
 };
