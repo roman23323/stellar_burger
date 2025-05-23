27914a77fd08ace28456f48391591e3c4c62ec6d
Roman <ignatenko_roman.s@mail.ru>
2025-05-23 11:00:30 +0300

    feat: add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,42 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Routes, Route } from 'react-router-dom';
 
 import { AppHeader } from '@components';
+import { Provider } from 'react-redux';
+import store from '../../services/store';
 
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <Provider store={store}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+
+        {/* ----- Защитить: ----- */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />}>
+          <Route path='orders' element={<ProfileOrders />} />
+        </Route>
+
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </Provider>
   </div>
 );
 
