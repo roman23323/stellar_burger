d9681ab6268a42ce6ba403d464802829e4d41035
Roman <ignatenko_roman.s@mail.ru>
2025-05-23 10:59:35 +0300

    feat: add auth slice, root reducer and configure store

---
+++ src/services/auth/slice.ts
@@ -0,0 +1,62 @@
+import { registerUserApi, TAuthResponse, TRegisterData } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TUser } from '@utils-types';
+import { RootState, useSelector } from '../store';
+
+type TErrorResponse = {
+  rejectValue: string;
+};
+
+export const register = createAsyncThunk<
+  TAuthResponse,
+  TRegisterData,
+  TErrorResponse
+>('user/register', async (regData: TRegisterData, { rejectWithValue }) =>
+  registerUserApi(regData)
+    .then((res) => res)
+    .catch((err) => {
+      console.log(err);
+      return rejectWithValue(err.message);
+    })
+);
+
+export type TAuthState = {
+  user: TUser | null;
+  accessToken: string | null;
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  accessToken: null,
+  isLoading: false,
+  error: null
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.user;
+        state.accessToken = action.payload.accessToken;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload as string;
+      });
+  }
+});
+
+export const authReducer = authSlice.reducer;
+
+export const selectAuthError = (state: RootState) => state.auth.error;
+export const selectAuthLoading = (state: RootState) => state.auth.isLoading;
