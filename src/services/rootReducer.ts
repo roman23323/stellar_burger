bc9e7e95e4d9ff3f3ec9776eee686bd5a4b7323c
Roman <ignatenko_roman.s@mail.ru>
2025-05-26 11:48:21 +0300

    feat: implement burger constructor slice

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import { ingredientsReducer } from './slices/ingredientsSlice';
+import { constructorReducer } from './slices/constructorSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  ingredients: ingredientsReducer
+  ingredients: ingredientsReducer,
+  burgerConstructor: constructorReducer
 });
 
 export default rootReducer;
