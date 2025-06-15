1bdf136691859c2b809002020f0c6d9ddb9447b4
Roman <ignatenko_roman.s@mail.ru>
2025-06-15 16:15:31 +0300

    test: check that choosen ingredients added

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -2,7 +2,6 @@ describe('проверка конструктора бургера', () => {
   const bun_id = '643d69a5c3f7b9001cfa093c';
   const main_id = '643d69a5c3f7b9001cfa0941';
   const sauce_id = '643d69a5c3f7b9001cfa0942';
-  const ingredients_id = [bun_id, main_id, sauce_id];
 
   const BURGER_API_URL = Cypress.env('BURGER_API_URL');
 
@@ -25,13 +24,26 @@ describe('проверка конструктора бургера', () => {
   });
 
   it('должен добавлять ингредиенты по кнопке "добавить"', () => {
-    // Добавляем булочки, начинку и соус
-    ingredients_id.forEach((id) => {
+    // Добавляем начинку и соус
+    [main_id, sauce_id].forEach((id) => {
+      // Проверяем, что ингредиента изначально нет в конструкторе
+      cy.get(`[data-testid="constructor-el-${id}"]`).should('not.exist');
+
+      // Добавляем ингредиент
       cy.get(`[data-testid="ingredient-${id}"]`).find('button').click();
+
+      // Проверяем, что выбранный ингредиент появился в констркуторе
+      cy.get(`[data-testid="constructor-el-${id}"]`).should('exist');
     });
 
-    // Проверяем, что в коснтруктор бургера добавились ингредиенты
-    cy.get('[data-testid^="constructor-"]').should('have.length', 4);
-    cy.get('[data-testid^="constructor-bun-"').should('have.length', 2);
+    // Проверяем, что булочек изначально нет в конструкторе
+    cy.get('[data-testid^="constructor-bun-"]').should('not.exist');
+
+    // Добавляем булочки
+    cy.get(`[data-testid="ingredient-${bun_id}"]`).find('button').click();
+
+    // Проверяем, что в конструкторе появились выбранные булочки
+    cy.get(`[data-testid="constructor-bun-top-${bun_id}"]`).should('exist');
+    cy.get(`[data-testid="constructor-bun-bottom-${bun_id}"]`).should('exist');
   });
 });
